c9e2413265768010c1737fe8afd4247649ff352c
Bohdan Partyk <info@podebrady.org>
2023-10-19 18:01:47 +0200

    setState object vs setState function

--- src/App.js
+++ src/App.js
@@ -7,14 +7,94 @@ class App extends Component {
         super(props);
         this.state = {
             dude: 'Marceline the Vampire',
+            characters:  [
+                {
+                    id: 1,
+                    who: 'Name 1 it is long name',
+                    wat: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+                    cool: 12
+                },
+                {
+                    id: 2,
+                    who: 'Name 2',
+                    wat: 'Lorem ipsum dolor',
+                    cool: 15
+                },
+                {
+                    id: 3,
+                    who: 'Name 3',
+                    wat: 'Lorem ipsum dolor sit amet, consectetuer.',
+                    cool: 42
+                }
+            ],
         };
+    }
+
+    handleChange = event => {
+        this.setState({
+            dude: event.target.value,
+        });
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        const newDude = {
+            id: 99,
+            who: this.state.dude,
+            wat: this.state.dude,
+            col: 19
+        }
+
+        // zapis do state je async a neni dobry mutaci Objektu, pole použit tento způsob. Tento způsobje pro primitivni typy
+        /* this.setState({
+            this.state.characters = [...state.characters, newDude];
+        }); */
+
+        this.setState(state => {
+            state.characters = [...state.characters, newDude];
+        });
+    }
+
+    /**
+     * @returns {unknown[]}
+     */
+    listOfDudes = () => {
+        return this.state.characters.map(dude => (
+            <li
+                className={dude.who.split(' ').length < 3 ? 'strong' : ''}
+                key={dude.id}
+            >
+                {dude.who}
+                {dude.who.split(' ').length < 3 && (
+                    <small>
+                        <strong>
+                            - lol, short name
+                        </strong>
+                    </small>
+                )}
+            </li>
+        ));
     }
 
     render() {
         return (
             <div>
-                <p>
-                    {this.state.dude} lives!
+                <ul>
+                    {this.listOfDudes()}
+                </ul>
+                <form
+                    className="add-new"
+                    onSubmit={this.handleSubmit}
+                >
+                    <input
+                        type="text"
+                        onChange={this.handleChange}
+                        value={this.state.dude}
+                    />
+                </form>
+                <p className="preview">
+                    {this.state.dude}
                 </p>
             </div>
         )
